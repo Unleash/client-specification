7110eb0d15eede51d918832a2470a61c65be3de2
ivaosthu <ivar.conradi.osthus@finn.no>
2018-06-19 19:13:33 +0200

    fix lint

--- schema/features-schema.js
+++ schema/features-schema.js
@@ -1,16 +1,22 @@
 const Joi = require('joi');
 
 const schema = Joi.object().keys({
-    version: Joi.number().min(1).required(),
-    features: Joi.array().items(Joi.object().keys({
-        name: Joi.string().required(),
-        description: Joi.string().optional(),
-        enabled: Joi.boolean().required(),
-        strategies: Joi.array().items(Joi.object().keys({
+    version: Joi.number()
+        .min(1)
+        .required(),
+    features: Joi.array().items(
+        Joi.object().keys({
             name: Joi.string().required(),
-            parameters: Joi.object()
-        }))
-    }))
+            description: Joi.string().optional(),
+            enabled: Joi.boolean().required(),
+            strategies: Joi.array().items(
+                Joi.object().keys({
+                    name: Joi.string().required(),
+                    parameters: Joi.object(),
+                })
+            ),
+        })
+    ),
 });
 
 module.exports = schema;
--- schema/test-case-schema.js
+++ schema/test-case-schema.js
@@ -4,16 +4,17 @@ const featuresSchema = require('./features-schema.js');
 const schema = Joi.object().keys({
     name: Joi.string(),
     state: featuresSchema,
-    tests: Joi.array().items(Joi.object().keys({
-        description: Joi.string().required(),
-        context: Joi.object().keys({
-            userId: Joi.string(),
-            sessionId: Joi.string()
-        }),
-        toggleName: Joi.string().required(),
-        expectedResult: Joi.boolean().required()
-    }))
+    tests: Joi.array().items(
+        Joi.object().keys({
+            description: Joi.string().required(),
+            context: Joi.object().keys({
+                userId: Joi.string(),
+                sessionId: Joi.string(),
+            }),
+            toggleName: Joi.string().required(),
+            expectedResult: Joi.boolean().required(),
+        })
+    ),
 });
-
 
 module.exports = schema;
