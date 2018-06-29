3e1f555868cc68e5d6fc0f2278b980b6b7ffb0ba
ivaosthu <ivar.conradi.osthus@finn.no>
2018-06-29 09:47:38 +0200

    Some structure and travis

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const Joi = require('joi');
 const chalk = require('chalk');
 
 const testCaseSchema = require('./schema/test-case-schema');
-const testCases = require('./tests.json');
+const testCases = require('./specifications/index.json');
 
 testCases.forEach(name => {
-    const testCase = require(`./${name}`);
+    const testCase = require(`./specifications/${name}`);
     Joi.validate(testCase, testCaseSchema, err => {
         if (err) {
             console.error(`${name} ${chalk.red('is not valid')}. \n`, err);
