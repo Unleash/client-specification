f72c9d1b5f1cd17fdca72b4a8b984dfc614e238d
Simon Hornby <liquidwicked64@gmail.com>
2025-02-05 11:07:13 +0200

    feat: add support for deltas api in the tests

--- schema/features-schema.js
+++ schema/features-schema.js
@@ -1,85 +1,11 @@
 const Joi = require('joi');
+const featureSchema = require('./feature-schema');
+const segmentSchema = require('./segment-schema');
 
-const schema = Joi.object().keys({
-    version: Joi.number()
-        .min(1)
-        .required(),
-    features: Joi.array().items(
-        Joi.object().keys({
-            name: Joi.string().required(),
-            description: Joi.string().optional(),
-            enabled: Joi.boolean().required(),
-            dependencies: Joi.array().optional().items(
-                Joi.object().keys({
-                    feature: Joi.string().required(),
-                    enabled: Joi.bool().optional(),
-                    variants: Joi.array().items(Joi.string()).optional()
-                })
-            ),
-            strategies: Joi.array().items(
-                Joi.object().keys({
-                    name: Joi.string().required(),
-                    parameters: Joi.object(),
-                    variants: Joi.array().items(
-                        Joi.object().keys({
-                            name: Joi.string().required(),
-                            payload: Joi.object().required().keys({
-                                type: Joi.string().required(),
-                                value: Joi.string().required().allow(""),
-                            }).optional(),
-                            weight: Joi.number().min(0).max(100000),
-                            stickiness: Joi.string().optional(),
-                        })).optional(),
-                    constraints: Joi.array().items(
-                        Joi.object().keys({
-                            contextName: Joi.string().required(),
-                            operator: Joi.string().required(),
-                            values: Joi.array().items(Joi.string()),
-                            caseInsensitive: Joi.bool().optional(),
-                            inverted: Joi.bool().optional(),
-                            value: Joi.string().optional()
-                        }).optional()
-                    ),
-                    segments: Joi.array().items(Joi.number()).optional()
-                })
-            ),
-            variants: Joi.array().items(
-                Joi.object().keys({
-                    name: Joi.string().required(),
-                    payload: Joi.object().required().keys({
-                        type: Joi.string().required(),
-                        value: Joi.string().required().allow(""),
-                    }).optional(),
-                    weight: Joi.number().min(0).max(100000),
-                    stickiness: Joi.string().optional(),
-                    overrides: Joi.array().items(
-                        Joi
-                            .object()
-                            .keys({
-                                contextName: Joi.string().required(),
-                                values: Joi.array().items(Joi.string()),
-                            })
-                            .optional()
-                    ),
-                })
-            )
-        })
-    ),
-    segments: Joi.array().items(
-        Joi.object().keys({
-            id: Joi.number().required(),
-            constraints: Joi.array().items(
-                Joi.object().keys({
-                    contextName: Joi.string().required(),
-                    operator: Joi.string().required(),
-                    values: Joi.array().items(Joi.string()),
-                    caseInsensitive: Joi.bool().optional(),
-                    inverted: Joi.bool().optional(),
-                    value: Joi.string().optional()
-                }).optional()
-            ),
-        }).optional()
-    )
+const featuresSchema = Joi.object().keys({
+    version: Joi.number().min(1).required(),
+    features: Joi.array().items(featureSchema),
+    segments: Joi.array().items(segmentSchema)
 });
 
-module.exports = schema;
+module.exports = featuresSchema;
